a66521a0dce4af1c9e27c6def1a6e27565c1816c
Simeon Tran <siatran@ucsc.edu>
2024-06-16 20:14:37 -0700

    Working on logic when receiving first packet.

--- examples/common/thread_border_router/include/handler.h
+++ examples/common/thread_border_router/include/handler.h
@@ -5,11 +5,12 @@
 
 typedef struct PacketLossStats
 {
+  bool receivedFirstPacket;
   struct timeval start;
   uint32_t nextSeqNumExpected;
   uint64_t packetsReceived;
   uint64_t packetsExpected;
-} Stats;
+} PacketLossStats;
 
 void defaultRequestHandler(void* aContext,
                            otMessage *aMessage,
--- examples/common/thread_border_router/src/packet_loss.c
+++ examples/common/thread_border_router/src/packet_loss.c
@@ -1,5 +1,14 @@
 #include "handler.h"
 
+static PacketLossStats stats =
+{
+  false,      // receivedFirstPacket
+  0,          // start
+  0,          // nextSeqNumExpected
+  0,          // packetsReceived
+  0           // packetsExpected
+};
+
 /**
  * TODO:
  *  1. When receiving the first packet, record the START TIME.
@@ -13,11 +22,14 @@ void packetLossRequestHandler(void* aContext,
                               otMessage *aMessage,
                               const otMessageInfo *aMessageInfo)
 {
-  printRequest(aMessage, aMessageInfo);
+  if (!stats.receivedFirstPacket) {
+    stats.receivedFirstPacket = true;
+    stats.start = getTimevalNow();
+  }
 
+  printRequest(aMessage, aMessageInfo);
   uint32_t sequenceNum = 0;
   getPayload(aMessage, (void *) &sequenceNum);
-
   otLogNotePlat("Packet has sequence number %" PRIu32 ".", sequenceNum);
 
   /**
