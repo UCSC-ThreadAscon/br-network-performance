ecc26921a0469fe0794223b0e506cc34a3c23a05
Simeon Tran <siatran@ucsc.edu>
2024-06-16 20:07:57 -0700

    Created struct to hold data needed to calculate packet loss.

--- examples/common/thread_border_router/include/handler.h
+++ examples/common/thread_border_router/include/handler.h
@@ -3,6 +3,14 @@
 #include "workload.h"
 #include "time_api.h"
 
+typedef struct PacketLossStats
+{
+  struct timeval start;
+  uint32_t nextSeqNumExpected;
+  uint64_t packetsReceived;
+  uint64_t packetsExpected;
+} Stats;
+
 void defaultRequestHandler(void* aContext,
                            otMessage *aMessage,
                            const otMessageInfo *aMessageInfo);
