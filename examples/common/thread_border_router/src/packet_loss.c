495dca6cbfccaa896f584a853340785e0bb1208a
Simeon Tran <siatran@ucsc.edu>
2024-06-18 15:29:48 -0700

    Initialized counters when receiving first packet.

--- examples/common/thread_border_router/src/packet_loss.c
+++ examples/common/thread_border_router/src/packet_loss.c
@@ -13,13 +13,17 @@ double getElpasedUs(struct timeval start)
   return (double) timeDiffMicro(start, getTimevalNow());
 }
 
-void printPacketInfo(otMessage *aMessage,
-                    const otMessageInfo *aMessageInfo,
-                    double timeReceivedUs)
-{
+uint32_t getSequenceNum(otMessage *aMessage) {
   uint32_t sequenceNum = 0;
   getPayload(aMessage, (void *) &sequenceNum);
+  return sequenceNum;
+}
 
+void printPacketInfo(otMessage *aMessage,
+                    const otMessageInfo *aMessageInfo,
+                    uint32_t sequenceNum,
+                    double timeReceivedUs)
+{
   printRequest(aMessage, aMessageInfo);
   otLogNotePlat("Packet has sequence number %" PRIu32 ".", sequenceNum);
   otLogNotePlat("The packet has been received at ~%.5f seconds.",
@@ -31,14 +35,20 @@ void packetLossRequestHandler(void* aContext,
                               otMessage *aMessage,
                               const otMessageInfo *aMessageInfo)
 {
+  uint32_t sequenceNum = getSequenceNum(aMessage);
+  double elapsedUs = getElpasedUs(stats.start);
+
   if (!stats.receivedFirstPacket) {
     stats.receivedFirstPacket = true;
     stats.start = getTimevalNow();
+
+    stats.packetsExpected = 1;
+    stats.packetsReceived = 1;
+    stats.nextSeqNumExpected = sequenceNum + 1;
   }
 
-  double elapsedUs = getElpasedUs(stats.start);
   if (elapsedUs <= PACKET_LOSS_DURATION_US) {
-    printPacketInfo(aMessage, aMessageInfo, elapsedUs);
+    printPacketInfo(aMessage, aMessageInfo, sequenceNum, elapsedUs);
 
     /** Calling sendCoapResponse() will not affect the Non-Confirmable tests,
      *  since the function will only ACK if the request is a GET or Confirmable.
