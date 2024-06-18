15778a446d488abcb41e7b49fd1ffb6396831d59
Simeon Tran <siatran@ucsc.edu>
2024-06-18 16:53:30 -0700

    Making good progress in fixing the bug.

--- examples/common/thread_border_router/src/packet_loss.c
+++ examples/common/thread_border_router/src/packet_loss.c
@@ -35,65 +35,59 @@ void packetLossRequestHandler(void* aContext,
                               otMessage *aMessage,
                               const otMessageInfo *aMessageInfo)
 {
-  otLogNotePlat("------------------");
-  printRequest(aMessage, aMessageInfo);
-  uint32_t sequenceNum = getSequenceNum(aMessage);
-  otLogNotePlat("The sequence number is %" PRIu32 ".", sequenceNum);
-  double elapsedUs = getElpasedUs(stats.start);
-
   if (stats.state == NotStarted) {
     stats.state = Counting;
     stats.start = getTimevalNow();
 
     stats.packetsExpected = 1;
     stats.packetsReceived = 1;
-    stats.nextSeqNumExpected = sequenceNum + 1;
+    stats.nextSeqNumExpected = getSequenceNum(aMessage) + 1;
   }
 
-  if (elapsedUs <= PACKET_LOSS_DURATION_US) {
-    printPacketInfo(aMessage, aMessageInfo, sequenceNum, elapsedUs);
+  if (stats.state == Counting) {
+    uint32_t sequenceNum = getSequenceNum(aMessage);
+    double elapsedUs = getElpasedUs(stats.start);
 
-    if (sequenceNum == stats.nextSeqNumExpected) {
-      // We got the packet we expected - there is no packet loss.
-      stats.packetsExpected += 1;
-      stats.packetsReceived += 1;
-      stats.nextSeqNumExpected = sequenceNum + 1;
+    if (elapsedUs <= PACKET_LOSS_DURATION_US) {
+      printPacketInfo(aMessage, aMessageInfo, sequenceNum, elapsedUs);
+
+      if (sequenceNum == stats.nextSeqNumExpected) {
+        // We got the packet we expected - there is no packet loss.
+        stats.packetsExpected += 1;
+        stats.packetsReceived += 1;
+        stats.nextSeqNumExpected = sequenceNum + 1;
+      }
+      else if (sequenceNum > stats.nextSeqNumExpected) {
+        // We lost a packet with sequence number "sequenceNum".
+        stats.packetsExpected += 1;
+        stats.nextSeqNumExpected = sequenceNum + 1;
+      }
+      else if (sequenceNum < stats.nextSeqNumExpected) {
+        // This packet arrived late. You assumed it was lost earlier,
+        // but actually, it is not lost.
+        stats.packetsReceived += 1;
+      }
+
+      /** Calling sendCoapResponse() will not affect the Non-Confirmable tests,
+       *  since the function will only ACK if the request is a GET or Confirmable.
+       */
+      sendCoapResponse(aMessage, aMessageInfo);
     }
-    else if (sequenceNum > stats.nextSeqNumExpected) {
-      // We lost a packet with sequence number "sequenceNum".
-      stats.packetsExpected += 1;
-      stats.nextSeqNumExpected = sequenceNum + 1;
-    }
-    else if (sequenceNum < stats.nextSeqNumExpected) {
-      // This packet arrived late. You assumed it was lost earlier,
-      // but actually, it is not lost.
-      stats.packetsReceived += 1;
-    }
-
-    /** Calling sendCoapResponse() will not affect the Non-Confirmable tests,
-     *  since the function will only ACK if the request is a GET or Confirmable.
-     */
-    sendCoapResponse(aMessage, aMessageInfo);
-  }
-  else {
-    if (stats.state == Counting) {
-      otLogNotePlat("Expected packets: %" PRIu64 ".", stats.packetsExpected);
-      otLogNotePlat("Packets Received: %" PRIu64 ".", stats.packetsReceived);
-
-      double packetLossRatio = ((double) stats.packetsReceived) /
-                               ((double) stats.packetsExpected); 
-      otLogNotePlat("Packet loss ratio: %.5f.", packetLossRatio);
-
+    else {
       stats.state = DisplayedResults;
     }
-    else if (stats.state == DisplayedResults) {
-      /** Packet loss has already been counted and displayed. The server
-       *  will not be doing anymore packet loss calculations.
-       */
-      stats.state = Finished;
-    }
   }
 
-  otLogNotePlat("------------------");
+  if (stats.state == DisplayedResults) {
+    double packetLossRatio = ((double) stats.packetsReceived) /
+                              ((double) stats.packetsExpected); 
+
+    otLogNotePlat("Expected packets: %" PRIu64 ".", stats.packetsExpected);
+    otLogNotePlat("Packets Received: %" PRIu64 ".", stats.packetsReceived);
+    otLogNotePlat("Packet loss ratio: %.5f.", packetLossRatio);
+
+    stats.state = Finished;
+  }
+
   return;
 }
