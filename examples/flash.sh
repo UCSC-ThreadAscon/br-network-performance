774a1547094a65f708ee98aa21fe6e4d23a83fe5
Simeon Tran <siatran@ucsc.edu>
2024-07-20 22:11:08 -0700

    Detect mismatch

--- examples/flash.sh
+++ examples/flash.sh
@@ -7,11 +7,16 @@ function get_cipher_flag() {
   cat $1/sdkconfig | grep CONFIG_THREAD_ASCON_CIPHER_SUITE | tail -c 2 | head -1
 }
 
-# Compare the encryption algorithm that is set in the sdkconfigs
-# of the RCP and Border Router. If they are different, throw an error.
-#
 rcp_cipher_flag=$(get_cipher_flag $rcp_path)
 border_router_cipher_flag=$(get_cipher_flag $border_router_path)
 
-echo "RCP Encryption Flag:           ${rcp_cipher_flag}"
-echo "Border Router Encryption Flag: ${border_router_cipher_flag}"
+# Compare the encryption algorithm that is set in the sdkconfigs
+# of the RCP and Border Router. If they are different, throw an error.
+#
+if [[ "$rcp_cipher_flag" != "$border_router_cipher_flag" ]]
+then
+  echo "ERROR: RCP and Border Router having an encryption algorithm mismatch!"
+  echo "RCP Encryption Flag:           ${rcp_cipher_flag}"
+  echo "Border Router Encryption Flag: ${border_router_cipher_flag}"
+  exit 1
+fi
