cf70d01f3d79752e685ae273a8c217e8deb53b65
Simeon Tran <siatran@ucsc.edu>
2024-07-20 23:17:32 -0700

    Use absolute path

--- examples/flash.sh
+++ examples/flash.sh
@@ -41,7 +41,7 @@ function flag_to_cipher_string () {
 . $HOME/esp/esp-idf/export.sh > /dev/null
 
 rcp_path="$IDF_PATH/examples/openthread/ot_rcp"
-border_router_path="./basic_thread_border_router"
+border_router_path="$HOME/Desktop/Repositories/br_throughput/examples/basic_thread_border_router"
 
 rcp_cipher_flag=$(get_cipher_flag $rcp_path)
 border_router_cipher_flag=$(get_cipher_flag $border_router_path)
