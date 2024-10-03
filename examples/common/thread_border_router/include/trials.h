bd6afdfc551f8b98934f2f4ae41ceac3b754c15d
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-03 12:25:30 -0700

    API to keept track of experimental trials.

---
+++ examples/common/thread_border_router/include/trials.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "openthread/logging.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+
+#include <stdint.h>
+#include <inttypes.h>
+
+void startMain(void);
+void startNextTrial(void);
