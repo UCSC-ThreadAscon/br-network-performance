a272a5548ebc49150328309cdecd31c5309621ca
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-11 14:14:06 -0700

    Add assertion to make sure all payload in throughput experiment are 4 bytes.

--- examples/common/thread_border_router/src/throughput.c
+++ examples/common/thread_border_router/src/throughput.c
@@ -1,9 +1,14 @@
 #include "handler.h"
 
+# define THROUGHPUT_EXP_PAYLOAD_SIZE 4
+
 void throughputRequestHandler(void* aContext,
                               otMessage *aMessage,
                               const otMessageInfo *aMessageInfo)
 {
+  uint16_t payloadLength = getPayloadLength(aMessage);
+  assert(payloadLength == THROUGHPUT_EXP_PAYLOAD_SIZE);
+
   sendCoapResponse(aMessage, aMessageInfo);
   return;
 }
