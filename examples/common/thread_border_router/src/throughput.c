9f99cacc2f7fca22190276847d55235818e85b2b
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-12 12:31:59 -0700

    Clarifying comment.

--- examples/common/thread_border_router/src/throughput.c
+++ examples/common/thread_border_router/src/throughput.c
@@ -7,6 +7,10 @@ void throughputRequestHandler(void* aContext,
                               const otMessageInfo *aMessageInfo)
 {
   uint16_t payloadLength = getPayloadLength(aMessage);
+
+  /** Make sure that all packets that are sent in the throughput
+   *  experiments have a payload of exactly 4 bytes.
+   */
   assert(payloadLength == THROUGHPUT_EXP_PAYLOAD_SIZE);
 
   sendCoapResponse(aMessage, aMessageInfo);
