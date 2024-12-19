cea226a571c3809b96580da02fe8a2acef4f0856
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-12-18 21:06:16 -0800

    Going to do experiment without identifying who is the leader.

--- examples/common/thread_border_router/src/start_server.c
+++ examples/common/thread_border_router/src/start_server.c
@@ -24,32 +24,20 @@ void startCoapServer(uint16_t port)
 #if EXPERIMENT_THROUGHPUT_UDP
 void expStartUdpServer(otDeviceRole role)
 {
-  if (role != OT_DEVICE_ROLE_LEADER)
-  {
-    EmptyMemory(&udpSocket, sizeof(otUdpSocket));
+  EmptyMemory(&udpSocket, sizeof(otUdpSocket));
 
-    otUdpReceive callback = NULL;
-    callback = tpUdpRequestHandler;
+  otUdpReceive callback = NULL;
+  callback = tpUdpRequestHandler;
 
-    handleError(otUdpOpen(OT_INSTANCE, &udpSocket, callback, NULL),
-                "Failed to open UDP socket.");
+  handleError(otUdpOpen(OT_INSTANCE, &udpSocket, callback, NULL),
+              "Failed to open UDP socket.");
 
-    udpSockAddr.mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
-    udpSockAddr.mPort = UDP_SOCK_PORT;
-    handleError(otUdpBind(OT_INSTANCE, &udpSocket, &udpSockAddr, OT_NETIF_THREAD),
-                "Failed to set up UDP server.");
-    
-    otLogNotePlat("Created UDP server at port %d.", UDP_SOCK_PORT);
-  }
-  else
-  {
-    PrintCritDelimiter();
-    otLogCritPlat("Border Router failed to attach to the Thread network lead by the FTD.");
-    otLogCritPlat("Going to restart the current experiment trial.");
-    PrintCritDelimiter();
-
-    esp_restart();
-  }
+  udpSockAddr.mAddress = *otThreadGetMeshLocalEid(OT_INSTANCE);
+  udpSockAddr.mPort = UDP_SOCK_PORT;
+  handleError(otUdpBind(OT_INSTANCE, &udpSocket, &udpSockAddr, OT_NETIF_THREAD),
+              "Failed to set up UDP server.");
+  
+  otLogNotePlat("Created UDP server at port %d.", UDP_SOCK_PORT);
   return;
 }
 #endif
